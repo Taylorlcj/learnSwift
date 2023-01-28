3349a5002a5ed84d0fcc0de68633047320d28d09
Taylor Jensen <Taylorlcj@gmail.com>
2023-01-27 21:44:52 -0500

    mac os app

---
+++ MacOS app/IOS Academy/IOS Academy/IOS_AcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_AcademyApp.swift
+//  IOS Academy
+//
+//  Created by Taylor Jensen on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct IOS_AcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
