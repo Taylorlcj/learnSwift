b98b3d96ebb98d77d634580d167bdafb1abaf2f0
Taylor Jensen <Taylorlcj@gmail.com>
2023-01-27 20:43:14 -0500

    commit this

---
+++ Xcode Tutorial Demo/Xcode Tutorial Demo/Xcode_Tutorial_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Tutorial_DemoApp.swift
+//  Xcode Tutorial Demo
+//
+//  Created by Taylor Jensen on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Tutorial_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
